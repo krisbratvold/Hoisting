1ecfd98755efb2af29c3a059c19d11476ac3d42b
krisbratvold <kris.bratvold@gmail.com>
2021-07-06 13:08:33 -0700

    initial commit

---
+++ hoist.js
@@ -0,0 +1,139 @@
+//#1
+
+// console.log(hello);                                   
+// var hello = 'world';
+
+// var hello;
+// console.log(hello);
+// hello = 'world';
+
+//#2
+
+// var needle = 'haystack';
+// test();
+// function test(){
+//     var needle = 'magnet';
+//     console.log(needle);
+// }
+
+// var needle;
+// test();
+// function test(){
+//     var needle;
+//     needle = 'magnet';
+//     console.log(needle);
+// }
+// needle = 'haystack';
+
+//#3
+
+// var brendan = 'super cool';
+// function print(){
+//     brendan = 'only okay';
+//     console.log(brendan);
+// }
+// console.log(brendan);
+
+// var brendan;
+// function print(){
+//     brendan = 'only okay';
+//     console.log(brendan);
+// }
+// brendan = 'super cool'
+// console.log(brendan);
+
+
+//#4
+
+// var food = 'chicken';
+// console.log(food);
+// eat();
+// function eat(){
+//     food = 'half-chicken';
+//     console.log(food);
+//     var food = 'gone';
+// }
+
+// var food;
+// food = 'chicken';
+// console.log(food);
+// eat();
+// function eat(){
+//     var food;
+//     food = 'half chicken';
+//     console.log(food);
+//     food = 'gone';
+// }
+
+//#5
+
+// mean();
+// console.log(food);
+// var mean = function() {
+//     food = "chicken";
+//     console.log(food);
+//     var food = "fish";
+//     console.log(food);
+// }
+// console.log(food);
+
+// var mean;
+// mean();
+// console.log(food);
+// mean = function(){
+//     var food;
+//     food= "chicken";
+//     console.log(food);
+//     food= "fish";
+//     console.log(food);
+// }
+// console.log(food);
+
+//#6
+
+// console.log(genre);
+// var genre = "disco";
+// rewind();
+// function rewind() {
+//     genre = "rock";
+//     console.log(genre);
+//     var genre = "r&b";
+//     console.log(genre);
+// }
+// console.log(genre);
+
+// var genre;
+// console.log(genre);
+// genre = "disco";
+// function rewind() {
+//     var genre;
+//     genre = "rock";
+//     console.log(genre);
+//     genre = "r&b";
+//     console.log(genre);
+// }
+// rewind();
+// console.log(genre);
+
+//#7
+
+// dojo = "san jose";
+// console.log(dojo);
+// learn();
+// function learn() {
+//     dojo = "seattle";
+//     console.log(dojo);
+//     var dojo = "burbank";
+//     console.log(dojo);
+// }
+// console.log(dojo);
+
+// function learn() {
+//     var dojo;
+//     dojo = "seattle";
+//     console.log(dojo);
+//     dojo = "burbank";
+//     console.log(dojo);
+// }
+// learn();
+// console.log(dojo);
